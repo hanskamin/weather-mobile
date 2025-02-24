596443b01f9ee2dfbe4a216085b12a823c7e43e3
Hans Kamin <hansjkamin@gmail.com>
2025-02-24 17:44:07 -0500

    Revert "added gluestack-ui"
    This reverts commit 4dcab9b85f92be1290b063b5955268e762ec09e6.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,5 @@
 import { Stack } from "expo-router";
 
-import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-
 export default function RootLayout() {
-  return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
+  return <Stack />;
 }
